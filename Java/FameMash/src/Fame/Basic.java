2edeef5b72096d259e4aae98c967d63c15f84146
NINJAMUSICIAN <j80mar4@gmail.com>
2014-03-31 18:58:51 -0600

    Pictures now display on website
    now to add some user input :)

--- Java/FameMash/src/Fame/Basic.java
+++ Java/FameMash/src/Fame/Basic.java
@@ -1,44 +1,66 @@
 package Fame;
 
-import java.io.*;
-import java.net.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.awt.image.*;
-import javax.imageio.*;
-import javax.swing.*;
+//49 is the best
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
 import java.util.Random;
+
+import javax.swing.JApplet;
 
 public class Basic extends JApplet {
 
+	private int NUMBER_OF_PICS = 173;
+	
 	public int x = 100, y = 200;
+	
+	
+	public Card card1, card2;
 	
 	public void Main(String[] args){
 		init();
+	
+		
 	}
 
 	public void init() {
 		setSize(800, 500);
+		card1 =makeCard();
+		card2 =makeCard();
+		
+		card2.setPosition(500, 100);
+		
 	}
 
+	public Card makeCard(){
+		Random r = new Random();
+		int sel = r.nextInt(NUMBER_OF_PICS + 1);
+		Card newCard = new Card(sel);
+		return newCard;
+	}
+	
 	public void paintBackground(Graphics g){
 		Dimension d = getSize();
 		g.setColor(Color.WHITE);
 		g.fillRect(0, 0, d.width, d.height);
 		g.setColor(Color.black);
+		Font font = new Font("Arial", Font.BOLD, 32);
+		g.setFont(font);
+		g.drawString("OR", 400, 250);
 	}
 
 	public void update(){
-		x++;
+	
 	}
 	
 	public void paint (Graphics g){
 		paintBackground(g);
 		update();
 		
-		
-		
-		repaint();
+		card1.draw(g);
+		card2.draw(g);
 	}
 
 }
--- Java/FameMash/src/Fame/Card.java
+++ Java/FameMash/src/Fame/Card.java
@@ -1,5 +1,42 @@
 package Fame;
 
-public class Card {
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
+import javax.imageio.ImageIO;
+
+public class Card {
+	
+	private static final int IMG_WIDTH = 225;
+	private static final int IMG_HEIGHT = 258;
+	
+	public BufferedImage rawPic;
+	public BufferedImage pic;
+	public int x = 100, y = 100;
+	private String rawVotes = "", rawViews = "";
+	private int votes = 0, views = 0;
+
+	
+	
+	public Card(int num){
+		String path = "/pic(" + num + ").jpg";
+		System.out.println(path);
+		try{
+			pic = ImageIO.read(getClass().getResourceAsStream(path));
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void setPosition(int x1, int y1){
+		x = x1;
+		y = y1;
+	}
+	
+	public void draw(Graphics g){
+		g.drawImage(pic, x, y, IMG_WIDTH, IMG_HEIGHT, null);
+	}
+	
 }
